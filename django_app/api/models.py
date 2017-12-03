ae7947a5f4b9e346e78cfd100df8bbf433733d41
xKxAxKx <pekepooooong@gmail.com>
2017-12-03 16:07:21 +0900

    [update]add models(Still on the way

--- django_app/api/models.py
+++ django_app/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class About(TimeStampedModel):
+    name = models.CharField(max_length=140, blank=True)
+    sub_name = models.CharField(max_length=140, blank=True)
+    real_name = models.CharField(max_length=140, blank=True)
+    detail = models.TextField(blank=True)
+
+
+class SNS(TimeStampedModel):
+    name = models.CharField(max_length=140)
+    account_name = models.CharField(max_length=140)
+    url = models.URLField()
+    # image = models.ImageField(upload_to='images/')
+
+
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=140)
+    url = models.URLField()
+    detail = models.TextField(blank=True)
+
+
+class Inquiry(TimeStampedModel):
+    name = models.CharField(max_length=140)
+    email = models.EmailField()
+    message = models.TextField(blank=True)
