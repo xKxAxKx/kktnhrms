834e9ca9bebdcc0398d8f478b23dc6aab0613dc5
kakutani <pekepooooong@gmail.com>
2017-12-28 18:20:32 +0900

    [update]add Profile.vue

---
+++ vue_front/src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    <h2>Profile</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
