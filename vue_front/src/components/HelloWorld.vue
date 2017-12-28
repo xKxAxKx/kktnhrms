1869050bbcb2a42cf39858ae17fdefb3e5714dd6
kakutani <pekepooooong@gmail.com>
2017-12-28 13:36:45 +0900

    [update]template

--- vue_front/src/components/HelloWorld.vue
+++ vue_front/src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Menu</h2>
+    <h2>Links(ここにSNSのリンクをforする)</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Profile</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Products</a></li>
-    </ul>
-    <h2>Links</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://router.vuejs.org/" target="_blank">Blog</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">Twitter</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <h2>Other</h2>
+    <ul>
+      <li><router-link to="/profile">Profile</router-link></li>
+      <li><router-link to="/products">Products</router-link></li>
     </ul>
   </div>
 </template>
