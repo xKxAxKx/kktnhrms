12721d68abb13f49cd25332e43122cd2ba943dd5
kakutani <pekepooooong@gmail.com>
2017-12-28 13:27:17 +0900

    [update]edit vuecomponent

--- vue_front/src/components/HelloWorld.vue
+++ vue_front/src/components/HelloWorld.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Menu</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="https://vuejs.org" target="_blank">Profile</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Products</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Links</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -25,7 +21,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'kktnhrms.com'
     }
   }
 }
