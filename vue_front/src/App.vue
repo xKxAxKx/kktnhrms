3d6390ff249c16866cb79cc8d89afbc684371652
kakutani <pekepooooong@gmail.com>
2017-12-28 18:21:28 +0900

    [update]

--- vue_front/src/App.vue
+++ vue_front/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1><router-link class="top-link" to="/">kktnhrms.com</router-link></h1>
     <router-view/>
   </div>
 </template>
@@ -18,5 +19,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1, h2, h3 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+.top-link:link {
+  color: #2c3e50;
+}
+.top-link:hover {
+  color: #2c3e50;
+}
+.top-link:visited {
+  color: #2c3e50;
+}
+.top-link:active {
+  color: #2c3e50;
 }
 </style>
