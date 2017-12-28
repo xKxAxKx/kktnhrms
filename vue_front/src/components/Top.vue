61d44558351dc03587127e66551560b6bafeac5e
kakutani <pekepooooong@gmail.com>
2017-12-28 18:19:40 +0900

    [update]rename main components

---
+++ vue_front/src/components/Top.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="top">
+    <h2>Link</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <br><br>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+    <h2>Other</h2>
+    <ul>
+      <li><router-link to="/profile">Profile</router-link></li>
+      <li><router-link to="/products">Products</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top',
+  data () {
+    return {
+    }
+  }
+}
+</script>
