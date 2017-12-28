fa9e58488ff3a8858b8a2bd4e6785f20c0737cb7
kakutani <pekepooooong@gmail.com>
2017-12-28 18:20:17 +0900

    [update]add Products.vue

---
+++ vue_front/src/components/Products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="products">
+    <h2>Products</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Products',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
