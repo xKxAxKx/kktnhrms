fcfa02ede9395d1246027267bb7359229db590d4
xKxAxKx <pekepooooong@gmail.com>
2018-01-03 14:51:37 +0900

    [update]profileのget

--- vue_front/src/components/Profile.vue
+++ vue_front/src/components/Profile.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="profile">
     <h2>Profile</h2>
+    <div class="detail">
+      <span v-html="profile.detail"></span>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Profile',
   data () {
     return {
+      profile: ''
     }
+  },
+  methods: {
+    fetchProfile: function() {
+      axios.get('http://127.0.0.1:8000/api/about/')
+           .then((res) => {
+             console.log(res.data)
+             this.profile = res.data
+           }).catch(error => {
+             console.log(error)
+           })
+    }
+  },
+  created: function () {
+    this.fetchProfile()
   }
 }
 </script>
+
+<style scoped>
+.detail {
+  display: inline-block;
+  text-align: left;
+}
+</style>
