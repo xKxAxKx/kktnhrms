d9d97d4a2c1bdc81ed4ddbe77d1ab327bcebc145
kakutani <pekepooooong@gmail.com>
2018-01-05 19:06:48 +0900

    [update]url

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from api.views import AboutGetView, SNSGetView, ProductGetView, InquiryPostView
 
 urlpatterns = [
-    path('about/', AboutGetView.as_view()),
-    path('sns/', SNSGetView.as_view({'get': 'list'})),
-    path('sns/<str:name>/', SNSGetView.as_view({'get': 'retrieve'})),
-    path('product/', ProductGetView.as_view({'get': 'list'})),
+    path('about/', AboutGetView.as_view(), name='about'),
+    path('sns/', SNSGetView.as_view({'get': 'list'}), name='sns_list'),
+    path('sns/<str:name>/', SNSGetView.as_view({'get': 'retrieve'}),
+         name='sns_get'),
+    path('product/', ProductGetView.as_view({'get': 'list'}),
+         name='product_list'),
     path('product/<int:product_id>/',
-         ProductGetView.as_view({'get': 'retrieve'})),
-    path('inquiry/', InquiryPostView.as_view())
+         ProductGetView.as_view({'get': 'retrieve'}),
+         name='product_get'),
+    path('inquiry/', InquiryPostView.as_view(),
+         name='inquiry')
 ]
