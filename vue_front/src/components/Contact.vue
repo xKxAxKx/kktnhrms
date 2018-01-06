9de50bb41686221f36b37fcf89f44747e93fd6e3
xKxAxKx <pekepooooong@gmail.com>
2018-01-06 18:12:09 +0900

    [update]update form

--- vue_front/src/components/Contact.vue
+++ vue_front/src/components/Contact.vue
@@ -4,18 +4,26 @@
     <form>
       <div class="form-group">
         <label>Name</label>
-        <input type="text" class="form-control" placeholder="Your Name">
+        <p v-if="confirm">{{ name }}</p>
+        <input v-else v-model="name" type="text" class="form-control" placeholder="Your Name">
       </div>
       <div class="form-group">
         <label>Email</label>
-        <input type="email" class="form-control" placeholder="example@example.com">
+        <p v-if="confirm">{{ email }}</p>
+        <input v-else v-model="email" type="email" class="form-control" placeholder="example@example.com" >
       </div>
       <div class="form-group">
         <label>Message</label>
-        <textarea class="form-control" rows="3"></textarea>
+        <div v-if="confirm">
+          {{ message }}
+        </div>
+        <textarea v-else v-model="message" class="form-control" rows="3"></textarea>
       </div>
-      <button type="submit" class="btn btn-success">Submit</button>
+      <button v-on:click="sendMessage" v-if="confirm" class="btn btn-success">Send Message</button>
+      <button v-on:click="canselConfirm" v-if="confirm" class="btn btn-danger">Cansel</button>
+      <button v-on:click="confirmMessage" v-else class="btn btn-success">Confirm</button>
     </form>
+    <p v-if='thanksMessage'>{{ thanksMessage }}</p>
   </div>
 </template>
 
@@ -24,6 +32,27 @@ export default {
   name: 'Contact',
   data () {
     return {
+      name: '',
+      email: '',
+      message: '',
+      confirm: false,
+      thanksMessage: ''
+    }
+  },
+  methods: {
+    confirmMessage: function () {
+      this.confirm = true
+    },
+    canselConfirm: function () {
+      this.confirm = false
+    },
+    sendMessage: function () {
+      console.log(this.name, this.email, this.message)
+      this.confirm = false
+      this.name = ''
+      this.email = ''
+      this.message = ''
+      this.thanksMessage = 'Thanks for the message!'
     }
   }
 }
