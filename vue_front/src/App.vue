637a1e00928d2d03b6a6fdfbad1c833d9b2cc55b
xKxAxKx <pekepooooong@gmail.com>
2018-01-06 17:02:35 +0900

    [update]form作成中

--- vue_front/src/App.vue
+++ vue_front/src/App.vue
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  max-width: 900px;
+  max-width: 500px;
   margin: auto;
 }
 h1, h2, h3 {
--- vue_front/src/components/Contact.vue
+++ vue_front/src/components/Contact.vue
@@ -2,13 +2,19 @@
   <div class="contact">
     <h2>Contact</h2>
     <form>
-      <div class="form-group inquiry" style="max-width: 550px;">
+      <div class="form-group">
+        <label>Name</label>
         <input type="text" class="form-control" placeholder="Your Name">
-        <br>
-        <input type="email" class="form-control" placeholder="Your Email">
-        <br>
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="email" class="form-control" placeholder="example@example.com">
+      </div>
+      <div class="form-group">
+        <label>Message</label>
         <textarea class="form-control" rows="3"></textarea>
       </div>
+      <button type="submit" class="btn btn-success">Submit</button>
     </form>
   </div>
 </template>
